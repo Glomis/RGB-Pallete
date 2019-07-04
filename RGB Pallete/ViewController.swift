251ccc01972ad90a9965b35c915db28a877a58eb
MAx <max@max.ru>
2019-07-04 14:38:16 +0300

    Initial Commit

---
+++ RGB Pallete/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGB Pallete
+//
+//  Created by Max on 04/07/2019.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
